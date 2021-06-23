3456a5389c9c1bf1a6ba974449ce93260c9d567a
Jan Benda <jan.benda@uni-tuebingen.de>
2021-06-24 00:04:47 +0200

    [TeensyBoard] no with teensy 4 detection

--- src/TeensyBoard.h
+++ src/TeensyBoard.h
@@ -10,26 +10,29 @@
 #include <Arduino.h>
 
 
-// from https://arduino.stackexchange.com/questions/21137/arduino-how-to-get-the-board-type-in-code
+// From https://github.com/pedvide/ADC/blob/master/settings_defines.h
 
 #if defined(TEENSYDUINO) 
-  #if defined(__AVR_ATmega32U4__)
-    #define TEENSY20
-  #elif defined(__AVR_AT90USB1286__)       
-    #define TEENSY20PP
-  #elif defined(__MK20DX128__)       
+  #if defined(__MK20DX256__) // Teensy 3.1/3.2
+    #define TEENSY31
+  #elif defined(__MK20DX128__) // Teensy 3.0
     #define TEENSY30
-  #elif defined(__MK20DX256__)       
-    #define TEENSY32             // and Teensy 3.1 (obsolete)
-  #elif defined(__MKL26Z64__)       
+  #elif defined(__MKL26Z64__) // Teensy LC
     #define TEENSYLC
-  #elif defined(__MK64FX512__)
+  #elif defined(__MK64FX512__) // Teensy 3.5
     #define TEENSY35
-  #elif defined(__MK66FX1M0__)
+  #elif defined(__MK66FX1M0__) // Teensy 3.6
     #define TEENSY36
+  #elif defined(__IMXRT1062__) // Teensy 4.0/4.1
+    // They only differ in the number of exposed pins
+    #define TEENSY4
   #else
-    // How to figure out Teensy 4.0 and 4.1 ?
-    #error "Unknown Teensy board"
+    #error "Unknown Teensy board!"
+  #endif
+  #ifdef ARDUINO_TEENSY41
+    #define TEENSY41
+  #else
+    #define TEENSY40
   #endif
 #else
   #error "Non Teensy boards not supported"
