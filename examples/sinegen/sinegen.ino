c1e37e3c415fe588cfe70b813d6ae56d000c0a4f
Jan Benda <jan.benda@uni-tuebingen.de>
2021-11-12 00:24:22 +0100

    [sinegen] all parameters as variables

--- examples/sinegen/sinegen.ino
+++ examples/sinegen/sinegen.ino
@@ -2,6 +2,8 @@
 
 Waveform wave;
 
+float freq = 500.0;
+float ampl = 0.1;
 float ampls[] = {0.5, -1.0};
 float phases[] = {0.0, -1.0};
 
@@ -10,7 +12,7 @@ void setup() {
   while (!Serial && millis() < 2000) {};
   wave.setup(140000);
   wave.setHarmonics(ampls, phases);
-  wave.start(A21, 500.0, 0.5);
+  wave.start(A21, freq, ampl);
 }
 
 
