013ed7d5a7267f3094344f4fa7624a4fb113d976
Jan Benda <jan.benda@uni-tuebingen.de>
2023-08-10 13:17:31 +0200

    [PushButtons] added LED feedback

--- examples/pushbuttons/pushbuttons.ino
+++ examples/pushbuttons/pushbuttons.ino
@@ -1,22 +1,27 @@
 #include <PushButtons.h>
 
-int ButtonPin = 24;
+int ButtonPin = 30;
 
 PushButtons buttons;
 
-
 void onpress(int id) {
+  digitalWrite(LED_BUILTIN, HIGH);
   Serial.printf("\nPressed button %d\n", id);
 }
 
 
 void onrelease(int id) {
+  digitalWrite(LED_BUILTIN, LOW);
   Serial.printf("Released button %d\n", id);
 }
 
-
 void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, HIGH);
   Serial.begin(9600);
+  while (!Serial && millis() < 2000) {};
+  Serial.println("PushButtons test");
+  digitalWrite(LED_BUILTIN, LOW);
   buttons.add(ButtonPin, INPUT_PULLUP, onpress, onrelease);
 }
 
