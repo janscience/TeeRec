deb70150d4ddcef609906c5284b1c699421de225
Jan Benda <jan.benda@uni-tuebingen.de>
2024-10-10 22:50:14 +0200

    [Settings] added setters

--- src/Settings.cpp
+++ src/Settings.cpp
@@ -15,3 +15,45 @@ Settings::Settings(const char *path, int deviceid, const char *filename,
   SensorsInterval(*this, "SensorsInterval", sensorsinterval, 0.001, 1e8, "%.1f", "s") {
   ID.setSpecial(0, "device");
 }
+
+
+void Settings::setPath(const char *path) {
+  Path.setValue(path);
+}
+
+
+void Settings::setDeviceID(int id) {
+  ID.setValue(id);
+}
+
+
+void Settings::setFileName(const char *fname) {
+  FileName.setValue(fname);
+}
+
+
+void Settings::setFileTime(float time) {
+  FileTime.setValue(time);
+}
+
+
+void Settings::setPulseFrequency(float freq) {
+  PulseFrequency.setValue(freq);
+}
+
+
+void Settings::setDisplayTime(float time) {
+  DisplayTime.setValue(time);
+}
+
+
+void Settings::setInitialDelay(float time) {
+  InitialDelay.setValue(time);
+}
+
+
+void Settings::setSensorsInterval(float time) {
+  SensorsInterval.setValue(time);
+}
+
+
--- src/Settings.h
+++ src/Settings.h
@@ -25,26 +25,50 @@ public:
   /* Path on SD card where to store the data. */
   const char *path() const { return Path.value(); };
 
+  /* Set path on SD card where to store the data to path. */
+  void setPath(const char *path);
+
   /* Device identifier. */
   int deviceID() const { return ID.value(); };
+
+  /* Set device identifier to id. */
+  void setDeviceID(int id);
 
   /* File name to be used to save the recorded data. */
   const char *fileName() const { return FileName.value(); };
 
+  /* Set name template to be used to save the recorded data to fname. */
+  void setFileName(const char *fname);
+
   /* Time in seconds the files will record data. */
   float fileTime() const { return FileTime.value(); };
+
+  /* Set time the files will record data to time seconds. */
+  void setFileTime(float time);
 
   /* Base frequency of generated test pulses. */
   float pulseFrequency() const { return PulseFrequency.value(); };
 
+  /* Set frequency of test pulses to freq Hertz. */
+  void setPulseFrequency(float freq);
+
   /* Time in seconds after which the display will be updated. */
   float displayTime() const { return DisplayTime.value(); };
+
+  /* Set update time for display to time seconds. */
+  void setDisplayTime(float time);
 
   /* Time in seconds until recording of data is started. */
   float initialDelay() const { return InitialDelay.value(); };
 
+  /* Set initial delay to time seconds. */
+  void setInitialDelay(float time);
+
   /* Time in seconds between sensor readings. */
   float sensorsInterval() const { return SensorsInterval.value(); };
+
+  /* Set time between sensor readings to time seconds. */
+  void setSensorsInterval(float time);
 
 
 protected:
