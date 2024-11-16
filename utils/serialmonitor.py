d462a0de4e732db2e05be423c30f507024cbfa1a
Jan Benda <jan.benda@uni-tuebingen.de>
2024-11-16 11:02:59 +0100

    [serialmonitor] first steps towards discovery of teensies

--- utils/serialmonitor.py
+++ utils/serialmonitor.py
@@ -1,11 +1,30 @@
-# pip install pyserial
 # https://github.com/pyserial/pyserial
-import serial
+try:
+    import serial
+    from serial.tools.list_ports import comports
+except ImportError:
+    print('ERROR: failed to import serial module !')
+    print('You need to install the pyserial package using')
+    print('> pip install pyserial')
+    exit()
 
-ser = serial.Serial('/dev/ttyACM0', 9600)
-ser.flushInput()
-ser.flushOutput()
 
-while True:
-    ser_bytes = ser.readline()
-    print(ser_bytes.decode('latin1').rstrip())
+def discover_teensy_ports():
+    for port in sorted(comports(False)):
+        print(port.device, port.name, port.description, port.hwid, port.vid, port.pid, port.serial_number, port.manufacturer, port.product)
+        # teensy36 = re.match(".*VID:PID=16C0:0483.*", hwid)
+
+
+def read_teensy():
+    ser = serial.Serial('/dev/ttyACM0', 9600)
+    ser.reset_input_buffer()
+    ser.reset_output_buffer()
+    while True:
+        ser_bytes = ser.readline()
+        print(ser_bytes.decode('latin1').rstrip())
+    
+
+if __name__ == '__main__':
+    #read_teensy()
+    discover_teensy_ports()
+    
