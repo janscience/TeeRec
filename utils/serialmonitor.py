4500f1aca5376f08f5e9730f1d898cccb4ba7968
Jan Benda <jan.benda@uni-tuebingen.de>
2024-11-18 17:22:43 +0100

    [serialmonitor] detect teensy model

--- utils/serialmonitor.py
+++ utils/serialmonitor.py
@@ -7,24 +7,82 @@ except ImportError:
     print('You need to install the pyserial package using')
     print('> pip install pyserial')
     exit()
+    
+# https://github.com/pyusb/pyusb
+# pip install pyusb
+try:
+    import usb.core
+    has_usb = True
+except ImportError:
+    has_usb = False
+
+
+teensy_model = {   
+    0x274: '30',
+    0x275: '31',
+    0x273: 'LC',
+    0x276: '35',
+    0x277: '36',
+    0x278: '40 beta',
+    0x279: '40',
+    0x280: '41',
+    0x281: 'MM'}
+"""Map bcdDevice of USB device to Teensy model version."""
 
 
 def discover_teensy_ports():
+    devices = []
+    serial_numbers = []
+    models = []
     for port in sorted(comports(False)):
-        print(port.device, port.name, port.description, port.hwid, port.vid, port.pid, port.serial_number, port.manufacturer, port.product)
-        # teensy36 = re.match(".*VID:PID=16C0:0483.*", hwid)
+        if port.vid is None and port.pid is None:
+            continue
+        #if port.vid == 0x16C0 and port.pid in [0x0483, 0x048B, 0x048C, 0x04D5]:
+        if port.manufacturer == 'Teensyduino':
+            teensy_model = get_teensy_model(port.vid, port.pid,
+                                            port.serial_number)
+            devices.append(port.device)
+            serial_numbers.append(port.serial_number)
+            models.append(teensy_model)
+    return devices, models, serial_numbers
 
 
-def read_teensy():
-    ser = serial.Serial('/dev/ttyACM0', 9600)
+def get_teensy_model(vid, pid, serial_number):
+    if has_usb:
+        dev = usb.core.find(idVendor=vid, idProduct=pid,
+                            serial_number=serial_number)
+        return teensy_model[dev.bcdDevice]
+    else:
+        return ''
+
+    
+def print_teensys(devices, models, serial_numbers):
+    for dev, model, num in zip(devices, models, serial_numbers):
+        print(f'Teensy{model} with serial number {num} on {dev:<20s}')
+
+        
+def read_teensy(device):
+    ser = serial.Serial(device)
     ser.reset_input_buffer()
     ser.reset_output_buffer()
     while True:
-        ser_bytes = ser.readline()
-        print(ser_bytes.decode('latin1').rstrip())
+        try:
+            ser_bytes = ser.readline()
+            print(ser_bytes.decode('latin1').rstrip())
+        except serial.serialutil.SerialException:
+            print()
+            print('disconnected')
+            break
     
 
 if __name__ == '__main__':
-    #read_teensy()
-    discover_teensy_ports()
+    print('Waiting for Teensy device ...')
+    while True:
+        devices, models, serial_numbers = discover_teensy_ports()
+        if len(devices) > 0:
+            break
+    print('found ', end='')
+    print_teensys(devices, models, serial_numbers)
+    read_teensy(devices[0])
+    
     
