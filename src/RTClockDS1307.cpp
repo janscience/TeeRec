f01ab7e6d8c33eccc7cf5044ea4422ea496c09ea
Jan Benda <jan.benda@uni-tuebingen.de>
2025-01-18 17:17:14 +0100

    [RTClockDS1307] check for temperature to identify DS3231 chip

--- src/RTClockDS1307.cpp
+++ src/RTClockDS1307.cpp
@@ -2,6 +2,9 @@
 #include <RTClockDS1307.h>
 
 
+#define DS1307_CTRL_ID 0x68
+
+
 RTClockDS1307::RTClockDS1307() :
   RTClock() {
 }
@@ -10,8 +13,22 @@ RTClockDS1307::RTClockDS1307() :
 bool RTClockDS1307::begin() {
   tmElements_t tm;
   if (RTC.read(tm) && RTC.chipPresent()) {
-    setI2CBus(Wire, 0x68);
+    setI2CBus(Wire, DS1307_CTRL_ID);
     setChip("DS1307/DS1337/DS3231");
+    // only DS3231 has temperature at 0x11:
+    Wire.beginTransmission(DS1307_CTRL_ID);
+    Wire.write((uint8_t)0x11); 
+    if (Wire.endTransmission() == 0) {
+      Wire.requestFrom(DS1307_CTRL_ID, 2);
+      if (Wire.available() == 2) {
+	int8_t temp1 = Wire.read(); 
+	Wire.read();
+	if (temp1 != 0) // let's assume a temperature != 0 ...
+	  setChip("DS3231");
+	else
+	  setChip("DS1307/DS1337");
+      }
+    }
     setSyncProvider(RTC.get);
     return true;
   }
